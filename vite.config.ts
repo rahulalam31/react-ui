5699d648bd5924c54b1c65b8888bc85eaf671866
rahulalam31 <gaberialcramern31@outlook.com>
2024-07-29 12:30:10 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { resolve } from "path";
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: {
     //library entry
     lib: {
