ecf9ad8698c6c70913c45826d3a18c949a46567d
rahulalam31 <gaberialcramern31@outlook.com>
2024-07-30 18:09:32 +0530

    src/app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,16 @@ function App() {
     <>
       <div>
         <Button>Test</Button>
-        <Button style='outline'>Test</Button>
-        <Button style='soft'>Test</Button>
+        <Button buttonStyle='outline'>Test</Button>
+        <Button buttonStyle='soft'>Test</Button>
 
-        <Alert type='danger'>LOL</Alert>
+        <Alert>LOL</Alert>
+        <Alert alertStyle='warning'>LOL</Alert>
+        <Alert alertStyle='error'>LOL</Alert>
+        <Alert alertStyle='success' iconImage='info'>LOL</Alert>
+        <Alert alertStyle='error' iconImage='error'>LOL</Alert>
+        <Alert alertStyle='warning' iconImage='warning'>LOL</Alert>
+        <Alert alertStyle='success' iconImage='success'>LOL</Alert>
       </div>
     </>
   )
