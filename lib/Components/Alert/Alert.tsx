03363d8727c6aba8f088269cc8c7240815e5f3e0
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2024-07-31 12:02:02 +0530

    Alert updated

--- lib/Components/Alert/Alert.tsx
+++ lib/Components/Alert/Alert.tsx
@@ -1,16 +1,66 @@
-import  { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 
 interface AlertProps {
-  children: ReactNode;
-  type?: "info" | "warning" | "danger";
-}
+	children: ReactNode;
+	onClick?: () => void;
+	alertStyle?: "info" | "error" | "success" | "warning";
+	size?: "base" | "lg" | "sm" | "xs";
+	iconOnly?: boolean;
+	className?: string;
+  }
 
-export const Alert: FC<AlertProps> = ({ children, type = "info" }) => {
-  const baseStyle = "px-4 py-2 rounded";
-  const styles = {
-    info: "bg-blue-100 text-blue-700",
-    warning: "bg-yellow-100 text-yellow-700",
-    danger: "bg-red-100 text-red-700",
-  };
-  return <div className={`${baseStyle} ${styles[type]}`}>{children}</div>;
+export const Alert: FC<AlertProps> = ({
+	children, onClick,
+	alertStyle = 'info',
+	size = 'base',
+	iconOnly = false,
+	className = '',
+	...props }) => {
+
+	const baseClass = 'aegov-alert';
+
+	const styleClass = {
+		info: 'alert-info',
+		error: 'alert-error',
+		success: 'alert-success',
+		warning: 'alert-warning'
+	}[alertStyle];
+
+	const sizeClass = {
+		base: 'btn-base',
+		lg: 'btn-lg',
+		sm: 'btn-sm',
+		xs: 'btn-xs'
+	}[size];
+
+	const iconClass = iconOnly ? 'btn-icon' : '';
+	return (
+		<div
+			className={`${baseClass} ${styleClass} ${sizeClass} ${iconClass} ${className}`}
+			onClick={onClick}
+			{...props}
+		>
+      <div className="alert-content">
+			{children}
+      </div>
+		</div>
+	);
 };
+
+
+// import  { FC, ReactNode } from "react";
+
+// interface AlertProps {
+//   children: ReactNode;
+//   type?: "info" | "warning" | "danger";
+// }
+
+// export const Alert: FC<AlertProps> = ({ children, type = "info" }) => {
+//   const baseStyle = "px-4 py-2 rounded";
+//   const styles = {
+//     info: "bg-blue-100 text-blue-700",
+//     warning: "bg-yellow-100 text-yellow-700",
+//     danger: "bg-red-100 text-red-700",
+//   };
+//   return <div className={`${baseStyle} ${styles[type]}`}>{children}</div>;
+// };
